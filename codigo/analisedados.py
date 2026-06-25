51c622e8d4ada98d3cfa37c58502bc376346a70e
vitoriacamilo-ai <vitoria.camilo@ufpe.br>
2026-06-25 19:59:35 -0300

    Adiciona função para exibição de dados

--- codigo/analisedados.py
+++ codigo/analisedados.py
@@ -1,4 +1,33 @@
 print ("Olá mundooooo!")
 
-idade = 27 
-genero = 'F'
+# Programa de exemplo - FAP 2026
+
+print("=" * 30)
+print("Exemplo de Variáveis em Python")
+print("=" * 30)
+
+nome = "Vitória"
+idade = 27
+genero = "F"
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Gênero: {genero}")
+
+
+
+----------------------------------------------------
+
+def exibir_dados(nome, idade, genero):
+    print("=" * 30)
+    print("Dados da Pessoa")
+    print("=" * 30)
+    print(f"Nome: {nome}")
+    print(f"Idade: {idade}")
+    print(f"Gênero: {genero}")
+
+nome = "Vitória"
+idade = 27
+genero = "F"
+
+exibir_dados(nome, idade, genero)
